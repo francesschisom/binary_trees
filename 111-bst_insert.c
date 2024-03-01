98c0443c858de4faecbd57f499ddaa3fdd19b2bf
Francess Chisom <chisomonwuzulike@gmail.com>
2024-03-01 09:45:43 -0800

    Binary Trees

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,35 +1,48 @@
 #include "binary_trees.h"
-#include "limits.h"
 
 /**
- * is_bst_helper - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
- * @lo: The value of the smallest node visited thus far.
- * @hi: The value of the largest node visited this far.
+ * bst_insert - Inserts a value in a Binary Search Tree.
+ * @tree: A double pointer to the root node of the BST to insert the value.
+ * @value: The value to store in the node to be inserted.
  *
- * Return: If the tree is a valid BST, 1, otherwise, 0.
+ * Return: A pointer to the created node, or NULL on failure.
  */
-int is_bst_helper(const binary_tree_t *tree, int lo, int hi)
+bst_t *bst_insert(bst_t **tree, int value)
 {
+	bst_t *curr, *new;
+
 	if (tree != NULL)
 	{
-		if (tree->n < lo || tree->n > hi)
-			return (0);
-		return (is_bst_helper(tree->left, lo, tree->n - 1) &&
-			is_bst_helper(tree->right, tree->n + 1, hi));
-	}
-	return (1);
-}
+		curr = *tree;
 
-/**
- * binary_tree_is_bst - Checks if a binary tree is a valid binary search tree.
- * @tree: A pointer to the root node of the tree to check.
- *
- * Return: 1 if tree is a valid BST, and 0 otherwise
- */
-int binary_tree_is_bst(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-	return (is_bst_helper(tree, INT_MIN, INT_MAX));
-}
+		if (curr == NULL)
+		{
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
+		}
+
+		if (value < curr->n) /* insert in left subtree */
+		{
+			if (curr->left != NULL)
+				return (bst_insert(&curr->left, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->left = new);
+		}
+		if (value > curr->n) /* insert in right subtree */
+		{
+			if (curr->right != NULL)
+				return (bst_insert(&curr->right, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->right = new);
+		}
+	}
+	return (NULL);
+}}
